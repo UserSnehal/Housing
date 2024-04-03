4c66c1a5cdc40213333e888c1c5e3a647aa5695c
UserSnehal <156944124+UserSnehal@users.noreply.github.com>
2024-04-04 00:11:25 +0530

    Update SnehalPatil_v1.py

--- SnehalPatil_v1.py
+++ SnehalPatil_v1.py
@@ -1,3 +1,6 @@
+print("hello");
+
+
 # import standard libraries
 import numpy as np
 import pandas as pd
@@ -37,3 +40,175 @@ print("seaborn: ",sns.__version__)
 print("sklearn: ",sklearn.__version__)
 print("statsmodels: ",statsmodels.__version__)
 print("scipy: ",scipy.__version__)
+
+#increase font size of all elements of seaborn fonts to make reading easier
+sns.set(font_scale=1.5)
+# see the data with all columns
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows',500)
+pd.set_option('display.width', 1000)
+
+
+# read data into a first data frame
+house = pd.read_csv('train.csv')
+
+house.head()
+
+
+house = pd.read_csv('train.csv')
+house.head()
+
+
+house = pd.read_csv('train.csv')
+house.head()
+
+
+house = pd.read_csv('train.csv')
+house.head()
+
+
+house.shape
+
+
+
+house.isnull().sum().sum()
+
+
+# checked missing values in the columns
+columns_nan =  house.columns[house.isnull().any()]
+
+for c in columns_nan:
+    print(c, house[c].isnull().sum())
+
+
+#check the percentage of missing data to make decisions on working with the missing data
+nullval = pd.DataFrame(round(house.isnull().sum()/len(house.index)*100,2).sort_values(ascending=False),columns=["Null values in %"])
+nullval.index.name = 'Names of Feature'
+nullval.head(10)
+
+
+fig=house.plot.box(title='Show details', figsize=(80, 40), legend=True, fontsize=24)
+fig.axes.title.set_size(40)
+
+numerical_features=[feature for feature in house.columns if house[feature].dtypes!='object']
+for feature in numerical_features:
+    if feature=='class(target)':
+        pass
+    else:
+    
+        
+        house[feature].hist()
+        plt.xlabel(feature)
+        plt.legend()
+        plt.xticks(rotation=90)
+        plt.tight_layout()
+        plt.show()
+
+print(house['LowQualFinSF'].value_counts())
+print(house['PoolArea'].value_counts())
+print(house['MiscVal'].value_counts())
+print(house['3SsnPorch'].value_counts())
+
+
+house=house.drop(['LowQualFinSF','PoolArea','MiscVal','3SsnPorch'],axis=1)
+house.shape
+
+# re-check the amount of missing values in the columns
+columns_nan =  house.columns[house.isnull().any()]
+
+for c in columns_nan:
+    print(c, house[c].isnull().sum())
+
+#imputing the media
+house['LotFrontage']=house['LotFrontage'].fillna(house['LotFrontage'].median())
+house['GarageYrBlt']=house['GarageYrBlt'].fillna(house['GarageYrBlt'].median())
+house['MasVnrArea']=house['MasVnrArea'].fillna(house['MasVnrArea'].median())
+#categorical values: imputing the mode
+for col in ('MasVnrType', 'GarageCond', 'GarageType', 'GarageFinish','GarageQual','BsmtExposure', 'BsmtFinType2', 'BsmtFinType1','BsmtCond','BsmtQual', 'Electrical'):
+    
+    house[col] = house[col].fillna(house[col].mode()[0])
+
+house.isnull().values.any()
+
+
+plt.figure()
+sns.distplot(house['TotRmsAbvGrd'],color='b')
+plt.show()
+
+plt.figure()
+sns.distplot(house['OverallQual'],color='r')
+plt.show()
+
+#visualise the dependent variable
+plt.figure()
+sns.distplot(house['SalePrice'],color='r')
+plt.show()
+
+# IQR to remove the outlier
+ 
+cols = ['MSSubClass', 'LotFrontage', 'LotArea', 'OverallQual', 'OverallCond', 
+         'YearBuilt', 'YearRemodAdd', 'MasVnrArea', 'BsmtFinSF1', 'BsmtFinSF2', 'BsmtUnfSF', 'TotalBsmtSF', '1stFlrSF', '2ndFlrSF', 'GrLivArea', 'BsmtFullBath', 'BsmtHalfBath', 'FullBath', 'HalfBath', 'BedroomAbvGr', 'KitchenAbvGr', 'TotRmsAbvGrd', 'Fireplaces', 'GarageYrBlt', 'GarageCars', 'GarageArea', 'WoodDeckSF', 'OpenPorchSF','EnclosedPorch','ScreenPorch', 'MoSold', 'YrSold', 'SalePrice'] 
+
+Q1 = house[cols].quantile(0.05)
+Q3 = house[cols].quantile(0.95)
+IQR = Q3 - Q1
+
+house = house[~((house[cols] < (Q1 - 1.5 * IQR)) |(house[cols] > (Q3 + 1.5 * IQR))).any(axis=1)]
+
+plt.figure()
+sns.distplot(house['SalePrice'],color='r')
+plt.show()
+
+# Visualise the target variable -> SalePrice after transforming the sales price
+
+house['SalePrice'] = np.log1p(house['SalePrice'])
+
+plt.title('SalePrice')
+sns.distplot(house['SalePrice'], bins=10)
+plt.show()
+
+# all numeric (float and int) variables in the dataset
+house_num = house.select_dtypes(include=['float64', 'int64'])
+house_num.head()
+
+
+
+plt.figure(figsize=(10,5))
+sns.pairplot(house, x_vars=['MSSubClass','LotFrontage','LotArea'], y_vars='SalePrice',height=4, aspect=1,kind='scatter')
+sns.pairplot(house, x_vars=['OverallQual', 'OverallCond','MasVnrArea'], y_vars='SalePrice',height=4, aspect=1,kind='scatter')
+sns.pairplot(house, x_vars=['BsmtFinSF1', 'BsmtUnfSF','TotalBsmtSF'], y_vars='SalePrice',height=4, aspect=1,kind='scatter')
+sns.pairplot(house, x_vars=['1stFlrSF','2ndFlrSF', 'GrLivArea'], y_vars='SalePrice',height=4, aspect=1,kind='scatter')
+sns.pairplot(house, x_vars=['BsmtFullBath','FullBath', 'HalfBath'], y_vars='SalePrice',height=4, aspect=1,kind='scatter')
+sns.pairplot(house, x_vars=['BedroomAbvGr','TotRmsAbvGrd', 'Fireplaces'], y_vars='SalePrice',height=4, aspect=1,kind='scatter')
+sns.pairplot(house, x_vars=['GarageCars','GarageArea', 'WoodDeckSF'], y_vars='SalePrice',height=4, aspect=1,kind='scatter')
+plt.show()
+
+# plotting correlations on a heatmap
+# correlation matrix
+cor = house_num.corr()
+cor
+plt.figure(figsize=(50,40))
+plt.xticks(fontsize=30)
+plt.yticks(fontsize=30)
+
+# heatmap
+sns.heatmap(cor, cmap="YlGnBu", annot=True, fmt='.2f')
+plt.title("Correlation between Variables", fontsize=40)
+plt.show()
+house.drop(['1stFlrSF','TotRmsAbvGrd','GarageArea', 'GarageYrBlt'], axis=1,inplace=True)
+
+
+# sales price correlation matrix
+plt.figure(figsize=(40,30))
+plt.xticks(fontsize=30)
+plt.yticks(fontsize=30)
+n = 20 # number of variables which have the highest correlation with 'Sales price'
+
+corrmat = house.corr()
+
+cols = corrmat.nlargest(n, 'SalePrice')['SalePrice'].index
+#plt.figure(dpi=100)
+sns.heatmap(house[cols].corr(),annot=True)
+plt.title("Correlation between Top 20 Variables", fontsize=40)
+plt.show()
+
